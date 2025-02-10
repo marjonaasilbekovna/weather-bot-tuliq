7cf609bd65aec7b57c4fe8986cf969874d771500
marjonaasilbekovna <abdurahmonovamarjona007@gmail.com>
2025-02-10 18:59:20 +0300

    kod yangilandi

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -5,4 +5,4 @@ from aiogram.filters import Command
 #help commands
 @dp.message(Command("help"))
 async def help_commands(message:Message):
-    await message.answer("🔰 Buyruqlar... \n\nBotdan foydalanish uchun - '/start' - tugmasini bosing va sizga kerakli bo'lgan viloyatni tugmalardan tanlang. \n'/about'  - Bot haqida qisqacha ma'lumot.\n'/xabar' - Adminga muroojatlaringizni yozib qoldirishingiz mumkin. ")
+    await message.answer("🔰 Buyruqlar... \n\nBotdan foydalanish uchun - '/start' - tugmasini bosing va sizga kerakli bo'lgan viloyatni tugmalardan tanlang.Shahringi haqidagi ma'lumotni Eslatma eslatish uchun avval'Eslatma ⏰' tugmasini tanlang va o'zingizga kerakli eslatmani belgilang. Va vaqtini tug'ri formatta kiriting.\n\n\n'/about'  - Bot haqida qisqacha ma'lumot.\n'/xabar' - Adminga muroojatlaringizni yozib qoldirishingiz mumkin.")
